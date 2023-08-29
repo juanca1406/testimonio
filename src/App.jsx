0358d3fccacdcb7e807f2921eb01273eddbe60d6
juanm <juan1406ma@gmail.com>
2023-08-28 19:30:56 -0500

    usando props

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,16 @@ import Testimonio from './componentes/Testimonio';
 function App() {
   return (
     <div className="App">
-      <Testimonio />
+      <div className='contenedor-principal'>
+        <h1>Nuestro grupo de trabajo en cazuelas Esto es lo que dicen:</h1>
+        <Testimonio
+          nombre='Juan Carlos Martinez M'
+          pais='Colombia'
+          cargo='Ingeniero de software'
+          texto='Declaración que hace una persona para demostrar o asegurar la veracidad de un hecho por haber sido testigo de él.
+        "el diario de la mañana afirma que según testimonios de las últimas personas que han podido abandonar el país ocupado, el ejército iraquí está llevando a cabo un registro sistemático, casa por casa, en busca de ciudadanos estadounidenses"'
+          imagen='' />
+      </div>
     </div >
   );
 }
--- src/componentes/Testimonio.jsx
+++ src/componentes/Testimonio.jsx
@@ -1,15 +1,16 @@
 import React from "react";
+import "../style/Testimonio.css";
 
-function Testimonio() {
+function Testimonio(props) {
   return (
     <div className="contenedor-testimonio">
       <img className="imagen-testimonio"
         src={require("../imagen/peq.png")}
         alt="foto de cazuela" />
       <div className="contenedor-opinion">
-        <p className="contenedor-nombre">Juan Carlos Martinez M</p>
-        <p className="contenedor-cargo">Ingeniero de software en cazuelas</p>
-        <p className="Contenedor-texto">Seguir para mejor mas</p>
+        <p className="contenedor-nombre">{props.nombre} en {props.pais}</p>
+        <p className="contenedor-cargo">{props.cargo}</p>
+        <p className="Contenedor-texto">{props.texto}</p>
       </div>
     </div>
 
